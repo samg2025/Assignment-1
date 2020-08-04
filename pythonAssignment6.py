0978ed307685009ee2d19d282c6cbfdd557f04f6
samg2025 <68552798+samg2025@users.noreply.github.com>
2020-08-04 13:36:40 -0400

    Update pythonAssignment6.py

--- pythonAssignment6.py
+++ pythonAssignment6.py
@@ -1,4 +1,6 @@
-# 8. Create a new dictionary by concatenating the following two dictionaries:
+#Task Three: Data Structures (Q 8, 9 ,10)
+
+#8. Create a new dictionary by concatenating the following two dictionaries:
 # a={1:10,2:20}
 # b={3:30,4:40}
 # Expected Result: {1:10,2:20,3:30,4:40}
